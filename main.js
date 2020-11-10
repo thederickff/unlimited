80f5a4fcfe2663a56e1b771b7920db48b37133a7
derickfelix <derickfelix4321@gmail.com>
2020-11-10 09:49:03 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.maximize();
+  win.loadFile('index.html');
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
